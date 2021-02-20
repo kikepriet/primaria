ea6b2fffc945dca48d89d088019f4c8d3bc29551
kike priet <kikepriet@gmail.com>
2021-02-20 14:35:39 -0600

    probando la base de datos para el cuento

--- php/conexion.php
+++ php/conexion.php
@@ -5,8 +5,7 @@ $user = "root";
 $pass = "";
 $db = "primaria";
 
-$mysqli = new mysqli("localhost", "usuario", "contraseña", "basedatos");
-if ($mysqli->connect_errno) {
+$conexion = new mysqli($host, $user, $pass, $db);
+if ($conexion->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-echo $mysqli->host_info . "\n";
--- php/cuento.php
+++ php/cuento.php
@@ -1,15 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Proyecto primaria</title>
+    <link rel="stylesheet" href="../css/cuentos.css">
+</head>
+<body>
 
-include 'conexion.php';
-    
+<div class="container grande">
+    <?php 
+        include 'conexion.php';
+        header('Content-Type: text/html; charset=UTF-8');
+        $numero = rand(1, 10); //el máximo debe ser el número de cuentos en la db
+        $sql = "SELECT * FROM cuentos WHERE id = 1"; //sustituir id por $numero
+        $result = $conexion->query($sql);
+        $row = $result->fetch_assoc();
+        echo "<h1 class='mt-5 text-center display-1'>" . utf8_encode($row["titulo"]) . "</h1>";
+        echo "<br />";
+        echo "<span class='text-justify'>" . utf8_encode($row["contenido"]) . "</span>";
+        echo "<br />";
+        echo "<small><p class='text-right'>" . utf8_encode($row["autor"]) . "</p></small>";
+        $conexion->close();
+    ?>
+</div>
 
-$sql = "SELECT COUNT(*) total FROM avisos";
-$result = mysqli_query($mysqli, $sql);
-$fila = mysqli_fetch_assoc($result);
-$filas = $fila['total'];
-$max = intval($filas);
-$min = 1;
-$numero = rand($min, $max) : int;
-
-
-?>
+</body>
+</html>
