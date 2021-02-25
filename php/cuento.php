1297640a2509065def5f8e2fb1f5f8ecff3ab4fa
kike priet <kikepriet@gmail.com>
2021-02-25 01:35:15 -0600

    poniendo bordes a la tabla de numero.html

--- php/cuento.php
+++ php/cuento.php
@@ -7,8 +7,27 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Proyecto primaria</title>
     <link rel="stylesheet" href="../css/cuentos.css">
+    <link rel="stylesheet" href="../css/letras.css">
 </head>
 <body>
+
+    <!--NAV BAR-->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="../index.html">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ml-auto">
+        <a class="nav-link" href="../html/letras.html">letras </a>
+        <a class="nav-link" href="../html/numero.html">números</a>
+        <a class="nav-link" href="../html/tablaDel1.html">tablas</a>
+        <a class="nav-link" href="../html/operaciones.html">sumas</a>
+        <a class="nav-link" href="../php/cuento.php">lecturas</a>
+      </div>
+    </div>
+  </nav>
+  <!--/NAV BAR-->
 
 <div class="container grande">
     <?php 
